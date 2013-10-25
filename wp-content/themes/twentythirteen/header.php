d8eec637206bd438c7fb56bdde3993d19790047a
Matt Boyd <matt@sqwiggle.com>
2013-10-25 18:40:08 -0500

    fixed <a> for header height

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -34,8 +34,10 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</a>
 
 			<div id="navbar" class="navbar">
 				<div id="site-navigation" class="navigation main-navigation" role="navigation">
