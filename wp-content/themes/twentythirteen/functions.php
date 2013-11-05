229434f9b700bcab0a37719e5057a1f2a7d2e66c
Eric Bieller <ericbieller@gmail.com>
2013-11-05 14:50:57 -0800

    Added large og:image

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -529,12 +529,8 @@ function insert_image_src_rel_in_head() {
 	global $post;
 	if ( !is_singular()) //if it is not a post or a page
 		return;
-	if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
-		$default_image="http://example.com/image.jpg"; //replace this with a default image on your server or an image in your media library
-		echo '<meta property="og:image" content="' . $default_image . '"/>';
-	}
 	else{
-		$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+		$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
 		echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
 	}
 	echo "
