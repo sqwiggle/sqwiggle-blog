96b76d59bb98429c6123b7ed3ec3234b246a62db
Tom Moor <tom.moor@gmail.com>
2013-12-12 22:22:35 +0100

    Fixed footer links, removed mixpanel badge

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -19,19 +19,19 @@
 				    <div class="four columns">
 					<h5>Sqwiggle</h5>
 					<ul>
-					    <li><a href="http://www.sqwiggle.com/pricing">Pricing</a></li>
-					    <li><a href="http://www.sqwiggle.com/team">The Team</a></li>
-					    <li><a href="http://www.sqwiggle.com/comparison">Features</a></li>
-					    <li><a href="http://www.sqwiggle.com/press">Press &amp; Brand</a></li>
-					    <li><a href="http://www.sqwiggle.com/success-stories">Success Stories</a></li>
+					    <li><a href="https://www.sqwiggle.com/pricing">Pricing</a></li>
+					    <li><a href="https://www.sqwiggle.com/team">The Team</a></li>
+					    <li><a href="https://www.sqwiggle.com/comparison">Features</a></li>
+					    <li><a href="https://www.sqwiggle.com/press">Press &amp; Brand</a></li>
+					    <li><a href="https://www.sqwiggle.com/success-stories">Success Stories</a></li>
 					    <li><a href="https://angel.co/sqwiggle/jobs">Work at Sqwiggle</a></li>
 					</ul>
 				    </div>
 				    <div class="four columns">
 					<h5>Resources</h5>
 					<ul>
-					    <li><a href="/help">Help Center</a></li>
-					    <li><a href="/security">Security</a></li>
+					    <li><a href="https://www.sqwiggle.com/help">Help Center</a></li>
+					    <li><a href="https://www.sqwiggle.com/security">Security</a></li>
 					</ul>
 				    </div>
 				    <div class="four columns">
@@ -40,7 +40,7 @@
 					    <li><a href="https://twitter.com/sqwiggle">Twitter</a></li>
 					    <li><a href="https://www.facebook.com/sqwiggleinc">Facebook</a></li>
 					    <li><a href="https://plus.google.com/+Sqwiggle">Google+</a></li>
-					    <li><a href="http://www.youtube.com/sqwiggleinc">YouTube</a></li>
+					    <li><a href="https://www.youtube.com/channel/UC3jtOeGkIoOFQEHOVjO9Rpw">YouTube</a></li>
 					    <li><a href="http://www.linkedin.com/company/sqwiggle">LinkedIn</a></li>
 					</ul>
 				    </div>
@@ -57,8 +57,7 @@
 				</nav>
 				<div class="copyright sixteen columns">
 				    <a href="https://www.sqwiggle.com/"><img class="logo" src="https://sqwiggle-assets.s3.amazonaws.com/assets/logo-footer-584fe8f13467ec2b1ea813b32e66d083.png" width="143" height="38"></a>
-				    <p>&#169; 2013 Sqwiggle, Inc. All Rights Reserved. <a href="http://www.sqwiggle.com/terms">Terms of Service</a> &amp; <a href="http://www.sqwiggle.com/privacy">Privacy Policy</a></p>
-				    <a href="https://mixpanel.com/f/partner"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_blue.png" alt="Mobile Analytics"></a>
+				    <p>&#169; 2013 Sqwiggle, Inc. All Rights Reserved. <a href="https://www.sqwiggle.com/terms">Terms of Service</a> &amp; <a href="https://www.sqwiggle.com/privacy">Privacy Policy</a></p>
 				</div>
 			    </div>
 		</footer>
